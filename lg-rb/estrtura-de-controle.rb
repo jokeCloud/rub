cd6319392856fd73e9722a71e53adbc0c0dbc933
johansen masterson <medalhaemerson@gmail.com>
2023-02-02 19:36:28 -0300

    condicao

---
+++ lg-rb/estrtura-de-controle.rb
@@ -0,0 +1,24 @@
+puts 7.odd?()
+
+puts 4.even?
+
+if 1.odd?()
+    puts '1 é um número impar.'
+end
+
+numero = 1
+outro_numero = 20
+
+if numero == outro_numero
+    puts "os números #{numero} e #{outro_numero} foram testados"
+    puts 'os números são iguais'
+end
+
+numero = 1
+outro_numero = 20
+
+if numero == outro_numero
+    puts 'Os números são iguais'
+else
+    puts 'Os números são diferentes'
+end
